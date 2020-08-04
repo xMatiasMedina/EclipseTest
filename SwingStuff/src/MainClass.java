a37b0d7f62e057980eb72b33a73acca4ccb04bbd
Matias <matias14medina@gmail.com>
2020-08-04 15:40:57 -0300

    Window builder tests

--- SwingStuff/src/MainClass.java
+++ SwingStuff/src/MainClass.java
@@ -3,5 +3,7 @@ import javax.swing.JOptionPane;
 public class MainClass {
 	public static void main(String[]args) {
 		JOptionPane.showMessageDialog(null, "hi");
+		Window window = new Window();
+		window.windowStart();
 	}
 }
--- SwingStuff/src/Window.java
+++ SwingStuff/src/Window.java
@@ -1,8 +1,21 @@
 import javax.swing.JFrame;
 
+import org.junit.Test;
+
+@SuppressWarnings("serial")
 public class Window extends JFrame{
 	public void windowStart(){
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(0, 0, 500, 500);
-		
+		setLocation(0, 0);
+		setLayout(null);
+		setResizable(false);
+		setVisible(true);
 	}
+	
+	@Test
+	public void test001() {
+		System.out.println("HI");
+	}
+	
 }
