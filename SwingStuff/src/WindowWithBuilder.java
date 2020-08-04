c402bcac9ab2dadcddd4227092245897571e8a73
Matias <matias14medina@gmail.com>
2020-08-04 15:41:27 -0300

    Window builder tests

---
+++ SwingStuff/src/WindowWithBuilder.java
@@ -0,0 +1,55 @@
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+
+public class WindowWithBuilder extends JFrame {
+
+	private JPanel contentPane;
+	private JTextField textField;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					WindowWithBuilder frame = new WindowWithBuilder();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public WindowWithBuilder() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		textField = new JTextField();
+		textField.setBounds(6, 23, 130, 26);
+		contentPane.add(textField);
+		textField.setColumns(10);
+		
+		initButtons();
+	}
+	
+	private void initButtons() {
+		JButton btnNewButton = new JButton("Enter");
+		btnNewButton.setBounds(149, 23, 117, 29);
+		contentPane.add(btnNewButton);
+	}
+}
