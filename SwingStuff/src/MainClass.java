2b0cf452bb2547960a8a016798a3fe13517066d9
Matias <matias14medina@gmail.com>
2020-08-03 22:00:34 -0300

    First Commit

---
+++ SwingStuff/src/MainClass.java
@@ -0,0 +1,7 @@
+import javax.swing.JOptionPane;
+
+public class MainClass {
+	public static void main(String[]args) {
+		JOptionPane.showMessageDialog(null, "hi");
+	}
+}
---
+++ SwingStuff/src/Window.java
@@ -0,0 +1,8 @@
+import javax.swing.JFrame;
+
+public class Window extends JFrame{
+	public void windowStart(){
+		setBounds(0, 0, 500, 500);
+		
+	}
+}
